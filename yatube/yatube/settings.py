bf8e753e9948a01de83dd0e074e220e982fb2611
Максим Иванов <ima_maxx@mail.ru>
2022-12-05 20:59:52 +0700

    Спринт 4/13 → Тема 2/6: Управление пользователями → Урок 8/9 + Необязательное задание

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -122,6 +122,12 @@ LOGIN_REDIRECT_URL = 'posts:index'
 # LOGOUT_REDIRECT_URL = 'posts:index'
 
 
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails') 
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
