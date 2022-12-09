75dce2189711f34e3b27407b368c94155be43543
Максим Иванов <ima_maxx@mail.ru>
2022-12-09 18:34:35 +0700

    Спринт 4/13 → Тема 5/6: Django: дорабатываем проект → Урок 4/5

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     # Главная страница
     path('', views.index, name='index'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
+    # Профайл пользователя
+    path('profile/<str:username>/', views.profile, name='profile'),
+    # Просмотр записи
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,25 +1,69 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
-import datetime
+from .models import Post, Group, User
+
 
 POST_COUNT = 10
 
 
 def index(request):
-    posts = Post.objects.all()[:POST_COUNT]
+    post_list = Post.objects.all().order_by('-pub_date')
+    # Если порядок сортировки определен в классе Meta модели,
+    # запрос будет выглядеть так:
+    # post_list = Post.objects.all()
+    # Показывать по 10 записей на странице.
+    paginator = Paginator(post_list, POST_COUNT) 
+
+    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
+    page_number = request.GET.get('page')
+
+    # Получаем набор записей для страницы с запрошенным номером
+    page_obj = paginator.get_page(page_number)
+    # Отдаем в словаре контекста
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
-    template = 'posts/index.html'
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context) 
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:POST_COUNT]
+    posts = group.posts.all()
+    paginator = Paginator(posts, POST_COUNT)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
         'posts': posts,
+        'page_obj': page_obj,
     }
     template = 'posts/group_list.html'
     return render(request, template, context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    posts = author.posts.all()
+    posts_count = posts.count()
+    author_full_name = author.get_full_name
+    paginator = Paginator(posts, POST_COUNT)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'posts': posts,
+        'post_count': posts_count,
+        'author_full_name': author_full_name,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk = post_id)
+    posts_count = post.author.posts.count()
+    context = {
+        'post': post,
+        'post_count': posts_count,
+    }
+    return render(request, 'posts/post_detail.html', context)
